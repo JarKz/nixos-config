58586f9b6e3bdb5a837bc2abd3dc7704592a3014
Pavel <work.belyavsky.p.a@gmail.com>
2026-01-26 22:10:19 +0300

    Enhance the tinymist LSP experience in neovim

--- home/configs/nvim/lua/plugins/lsp/lspconfig.lua
+++ home/configs/nvim/lua/plugins/lsp/lspconfig.lua
@@ -80,6 +80,23 @@ return {
     servers = {
       tinymist = {
         cmd = { "{{ tinymist }}" },
+        on_attach = function(client, bufnr)
+          vim.keymap.set("n", "<leader>tp", function()
+            client:exec_cmd({
+              title = "pin",
+              command = "tinymist.pinMain",
+              arguments = { vim.api.nvim_buf_get_name(0) },
+            }, { bufnr = bufnr })
+          end, { desc = "[T]inymist [P]in", noremap = true })
+
+          vim.keymap.set("n", "<leader>tu", function()
+            client:exec_cmd({
+              title = "unpin",
+              command = "tinymist.pinMain",
+              arguments = { vim.v.null },
+            }, { bufnr = bufnr })
+          end, { desc = "[T]inymist [U]npin", noremap = true })
+        end,
       },
       pyright = {
         cmd = { "{{ pyright-langserver }}", "--stdio" },
