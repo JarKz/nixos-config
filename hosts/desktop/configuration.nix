d51df855aab8ae5b4dedc3dc8f5f007bda818156
Pavel <work.belyavsky.p.a@gmail.com>
2026-01-26 22:20:07 +0300

    Re-structurize the nixos configuration to make unified form

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, ... }:
+{ ... }:
 {
   nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = [
@@ -8,7 +8,6 @@
 
   imports = [
     ./hardware-configuration.nix
-    ./gpu.nix
 
     ../../system
   ];
@@ -25,51 +24,6 @@
   };
 
   networking.hostName = "desktop";
-  networking.networkmanager.enable = true;
-
-  time.timeZone = "Europe/Minsk";
-
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
-    extraLocaleSettings = {
-      LC_TIME = "be_BY.UTF-8";
-    };
-  };
-
-  environment.systemPackages = lib.mkBefore (
-    with pkgs;
-    [
-      vim
-      wget
-    ]
-  );
-
-  services = {
-    upower.enable = true;
-
-    gvfs.enable = true;
-    udisks2.enable = true;
-  };
-
-  programs = {
-    niri.enable = true;
-    thunderbird.enable = true;
-    steam.enable = true;
-
-    gnupg.agent = {
-      enable = true;
-    };
-  };
-
-  virtualisation.docker = {
-    enable = true;
-
-    # We cannot use minikube with rootless docker. That's sad.
-    # rootless = {
-    #   enable = true;
-    #   setSocketVariable = true;
-    # };
-  };
 
   system.stateVersion = "25.05";
 }
--- system/default.nix
+++ system/default.nix
@@ -1,5 +1,53 @@
-{ ... }: {
+{ lib, pkgs, ... }:
+{
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/Minsk";
+
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_TIME = "be_BY.UTF-8";
+    };
+  };
+
+  environment.systemPackages = lib.mkBefore (
+    with pkgs;
+    [
+      vim
+      wget
+    ]
+  );
+
+  services = {
+    upower.enable = true;
+
+    gvfs.enable = true;
+    udisks2.enable = true;
+  };
+
+  programs = {
+    niri.enable = true;
+    thunderbird.enable = true;
+    steam.enable = true;
+
+    gnupg.agent = {
+      enable = true;
+    };
+  };
+
+  virtualisation.docker = {
+    enable = true;
+
+    # We cannot use minikube with rootless docker. That's sad.
+    # rootless = {
+    #   enable = true;
+    #   setSocketVariable = true;
+    # };
+  };
+
   imports = [
+    ./gpu.nix
     ./audio.nix
     ./bluetooth.nix
     ./deepcool.nix
